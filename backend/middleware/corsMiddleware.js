4e104a0b3c45aac2be0226b9d7a06909d781ff99
ganesh-6 <ganesh.22010331@viit.ac.in>
2025-04-01 12:02:23 +0530

    cors changes

--- backend/middleware/corsMiddleware.js
+++ backend/middleware/corsMiddleware.js
@@ -1,8 +1,19 @@
 import cors from "cors";
 
 export const corsMiddleware = cors({
-  origin: [/http:\/\/localhost(:\d+)?/, /http:\/\/192\.168\.29\.112(:\d+)?/, "https://hotel-three-psi.vercel.app/"],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  origin: [
+    /http:\/\/localhost(:\d+)?/, 
+    /http:\/\/192\.168\.29\.112(:\d+)?/, 
+    "https://hotel-three-psi.vercel.app",
+    "https://hotel-oeba.onrender.com"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: [
+    "Content-Type", 
+    "Authorization",
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Origin"
+  ],
+  exposedHeaders: ["Authorization"],
   credentials: true,
 });
